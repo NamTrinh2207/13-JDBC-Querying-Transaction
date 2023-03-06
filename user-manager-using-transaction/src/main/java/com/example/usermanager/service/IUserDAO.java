d44a0dc556b700ed54eb05ec49d3f8507b4a1016
NamTrinh2207 <trinhnam180697@gmail.com>
2023-03-07 02:03:14 +0700

    [Thực hành] Thực thi SQL có sử dụng Transaction

---
+++ user-manager-using-transaction/src/main/java/com/example/usermanager/service/IUserDAO.java
@@ -0,0 +1,19 @@
+package com.example.usermanager.service;
+
+import com.example.usermanager.model.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IUserDAO {
+     void insertUser(User user) throws SQLException;
+
+     User selectUser(int id) throws SQLException;
+
+     List<User> selectAllUsers() throws SQLException;
+
+     void deleteUser(int id) throws SQLException;
+
+     void updateUser(User user) throws SQLException;
+     void insertUpdateUseTransaction();
+}
