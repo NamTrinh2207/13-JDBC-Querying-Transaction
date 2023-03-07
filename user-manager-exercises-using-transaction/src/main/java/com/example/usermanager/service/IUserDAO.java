cf6ec4494f7e428ce07fda9ddb51ee1ee6a1a913
NamTrinh2207 <trinhnam180697@gmail.com>
2023-03-07 15:18:32 +0700

    [Bài tập] Luyện tập MySql JDBC Transaction

---
+++ user-manager-exercises-using-transaction/src/main/java/com/example/usermanager/service/IUserDAO.java
@@ -0,0 +1,18 @@
+package com.example.usermanager.service;
+
+import com.example.usermanager.model.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IUserDAO {
+     void addUserTransaction(User user) throws SQLException;
+
+     User selectUser(int id) throws SQLException;
+
+     List<User> selectAllUsers() throws SQLException;
+
+     void deleteUser(int id) throws SQLException;
+
+     void updateUser(User user) throws SQLException;
+}
